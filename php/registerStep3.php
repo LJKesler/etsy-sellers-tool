a2656e57e40b7c4eb27590c94a07d9cec9a54fc1
LJKesler <ljkesler@gmail.com>
2013-03-25 10:56:11 -0300

    Update registerStep3.php

--- php/registerStep3.php
+++ php/registerStep3.php
@@ -5,14 +5,14 @@ require 'db.inc.php';
 	$request_token = $_GET['oauth_token'];
 	$request_token_secret = $_COOKIE['requestTokenSecret'];
 
-	$oauth = new OAuth(OAUTH_CONSUMER_KEY, OAUTH_CONSUMER_SECRET);
-	$oauth->disableSSLChecks();
-	$oauth->setToken($request_token, $request_token_secret);
+	$oauthStep2 = new OAuth(OAUTH_CONSUMER_KEY, OAUTH_CONSUMER_SECRET);
+	$oauoauthStep2th->disableSSLChecks();
+	$oauthStep2->setToken($request_token, $request_token_secret);
 
 	$verifier = $_GET['oauth_verifier'];
 
 	try {
-	    $acc_token = $oauth->getAccessToken("https://sandbox.openapi.etsy.com/v2/oauth/access_token", null, $verifier);
+	    $acc_token = $oauthStep2->getAccessToken("https://sandbox.openapi.etsy.com/v2/oauth/access_token", null, $verifier);
 	    $access_token_secret = $acc_token['oauth_token_secret'];
 	    $access_token = $acc_token['oauth_token'];
 	    
@@ -21,26 +21,26 @@ require 'db.inc.php';
 	} catch (OAuthException $e) {
 		error_log('Step 2 exception');
 	    error_log($e->getMessage());
-	    error_log(print_r($oauth->getLastResponse(), true));
-	    error_log(print_r($oauth->getLastResponseInfo(), true));
+	    error_log(print_r($oauthStep2->getLastResponse(), true));
+	    error_log(print_r($oauthStep2->getLastResponseInfo(), true));
 	    exit;
 	}
 
     $oauthStep3 = new OAuth(OAUTH_CONSUMER_KEY, OAUTH_CONSUMER_SECRET, OAUTH_SIG_METHOD_HMACSHA1, OAUTH_AUTH_TYPE_URI);
-	$oauthStep3->setToken($access_token, $access_token_secret);
+    $oauthStep3->setToken($access_token, $access_token_secret);
 
 try {
-    $data = $oauth->fetch("https://sandbox.openapi.etsy.com/v2/oauth/scopes", null, OAUTH_HTTP_METHOD_GET);
-    $json = $oauth->getLastResponse();
+    $data = $oauthStep3->fetch("https://sandbox.openapi.etsy.com/v2/oauth/scopes", null, OAUTH_HTTP_METHOD_GET);
+    $json = $oauthStep3->getLastResponse();
     print_r(json_decode($json, true));
     
 } catch (OAuthException $e) {
 	error_log("Step 3 Error :: \n");
     error_log($e->getMessage());
-    error_log(print_r($oauth->getLastResponse(), true));
-    error_log(print_r($oauth->getLastResponseInfo(), true));
+    error_log(print_r($oauthStep3->getLastResponse(), true));
+    error_log(print_r($oauthStep3->getLastResponseInfo(), true));
     print "\nERROR :: " . $e->getMessage() . "\n";
-    print "\nlast response :: " . $oauth->getLastResponse();
+    print "\nlast response :: " . $oauthStep3->getLastResponse();
     exit;
 }
 
