e07619a83f2a77927bed921b602d10e20f3e3f84
LJKesler <ljkesler@gmail.com>
2013-03-26 15:50:24 -0300

    Update registerStep3.php

--- php/registerStep3.php
+++ php/registerStep3.php
@@ -1,6 +1,20 @@
 <?php
 	require 'oauth.inc.php';
 	require 'db.inc.php';
+	require 'url.inc.php';
+	
+	$baseURL;
+	switch (ENV){
+		case "\"DEV\"":
+			$baseURL = SITE_DEV_BASE_URL;
+			break;
+		case "\"LOCAL\"":
+			$baseURL = SITE_LOCAL_BASE_URL;
+			break
+		case "\"PROD\"":
+			$baseURL = SITE_PROD_BASE_URL:
+			break;
+	}
 
 	$request_token = $_GET['oauth_token'];
 	$request_token_secret = $_COOKIE['requestTokenSecret'];
@@ -12,7 +26,7 @@
 	$verifier = $_GET['oauth_verifier'];
 
 	$access_token_secret;
-    $access_token;
+    	$access_token;
 
 	try {
 	    $acc_token = $oauthStep2->getAccessToken("http://sandbox.openapi.etsy.com/v2/oauth/access_token", null, $verifier);
@@ -22,10 +36,9 @@
 	    $username = $_COOKIE['user'];
 	    $query = "UPDATE registered_users SET accessToken='".$access_token."', accessTokenSecret='" . $access_token_secret . "' where username = '". $username."';";
 	    $insertTokenQueryResult = mysqli_query($link, $query);
-	    header("Location: http://localhost:8888/main.html");
+	    header("Location: " . $baseURL . "/main.html");
 	    exit;
 	} catch (OAuthException $e) {
-		error_log('Step 2 exception');
 	    error_log($e->getMessage());
 	    error_log(print_r($oauthStep2->getLastResponse(), true));
 	    error_log(print_r($oauthStep2->getLastResponseInfo(), true));
