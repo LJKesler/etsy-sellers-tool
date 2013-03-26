17025f7742a06f72b896fd1b0f7388d120398fec
LJKesler <ljkesler@gmail.com>
2013-03-26 15:21:32 -0300

    Create url.inc.php

---
+++ php/url.inc.php
@@ -0,0 +1,6 @@
+<?php
+define('SITE_LOCAL_DEV_BASE_URL', 'localhost:8888');
+define('SITE_PROD_BASE_URL', 'leighkesler.com/etsysellers');
+define('SITE_REMOTE_DEV_BASE_URL', 'leighkesler.com/etsydev');
+
+?>
