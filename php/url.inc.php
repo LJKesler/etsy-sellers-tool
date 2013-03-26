afc1affea4bde50a815a42311ba7a6d64d05bf86
LJKesler <ljkesler@gmail.com>
2013-03-26 15:42:18 -0300

    Update url.inc.php

--- php/url.inc.php
+++ php/url.inc.php
@@ -1,6 +1,10 @@
 <?php
-define('SITE_LOCAL_DEV_BASE_URL', 'localhost:8888');
-define('SITE_PROD_BASE_URL', 'leighkesler.com/etsysellers');
-define('SITE_REMOTE_DEV_BASE_URL', 'leighkesler.com/etsydev');
+define('ENV', "DEV");
 
-?>
+define('SITE_LOCAL_BASE_URL', 'http://localhost:8888');
+define('SITE_DEV_BASE_URL',   'http://leighkesler.com/etsydev');
+define('SITE_PROD_BASE_URL',  'http://leighkesler.com/etsysellers');
+
+define('CALLBACK_LOCAL_URL', 'http://localhost:8888/php/registerStep3.php');
+define('CALLBACK_DEV_URL',   'http://leighkesler.com/etsydev/php/registerStep3.php');
+define('CALLBACK_PROD_URL',  'http://leighkesler.com/etsysellers/php/registerStep3.php');
