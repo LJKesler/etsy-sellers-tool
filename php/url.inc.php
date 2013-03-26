8a2f604e2005f14f6c2b84d2a0fd972c3deef702
LJKesler <ljkesler@gmail.com>
2013-03-26 15:52:14 -0300

    Update url.inc.php

--- php/url.inc.php
+++ php/url.inc.php
@@ -8,3 +8,5 @@ define('SITE_PROD_BASE_URL',  'http://leighkesler.com/etsysellers');
 define('CALLBACK_LOCAL_URL', 'http://localhost:8888/php/registerStep3.php');
 define('CALLBACK_DEV_URL',   'http://leighkesler.com/etsydev/php/registerStep3.php');
 define('CALLBACK_PROD_URL',  'http://leighkesler.com/etsysellers/php/registerStep3.php');
+
+define('ETSY_SANDBOX_URL' , 'http://sandbox.openapi.etsy.com/v2");
